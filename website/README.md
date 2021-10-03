9aa708befb2ae9bb0ae3722f00650953b6ea5d2a
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-03 22:24:39 +0100

    Add website (copied on SWR)

---
+++ website/README.md
@@ -0,0 +1,18 @@
+# [rtg website](https://rtg.vercel.app)
+
+The official website for [rtg](https://github.com/rtg-project/rtg).
+
+https://stork-search.net/docs/interface
+
+To develop locally, clone this repository and run the following command to start the local dev server:
+
+```bash
+yarn
+yarn dev
+```
+
+And visit `localhost:3000` to preview your changes.
+
+## Contributors
+
+- https://github.com/rtg-project/rtg/graphs/contributors
---
+++ website/next.config.js
@@ -0,0 +1,62 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_stork: true,
+  unstable_staticImage: true,
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ["en-US", "zh-CN", "es-ES", "ja", "ko"],
+    defaultLocale: "en-US",
+  },
+  redirects: () => {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 301,
+      },
+      {
+        source: "/advanced/performance",
+        destination: "/docs/advanced/performance",
+        statusCode: 301,
+      },
+      {
+        source: "/advanced/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/change-log",
+        destination: "/docs/change-log",
+        statusCode: 301,
+      },
+      {
+        source: "/blog/rtg-1",
+        destination: "/blog/rtg-v1",
+        statusCode: 301,
+      },
+      {
+        source: "/blog",
+        destination: "/blog/rtg-v1",
+        statusCode: 302,
+      },
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 302,
+      },
+      {
+        source: "/examples",
+        destination: "/examples/basic",
+        statusCode: 302,
+      },
+    ];
+  },
+});
