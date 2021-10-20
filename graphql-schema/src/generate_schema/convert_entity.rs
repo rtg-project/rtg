ba0cc3312b41532c5d7c1e212b9682c11c527550
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-21 00:20:14 +0100

    Add Node

--- graphql-schema/src/generate_schema/convert_entity.rs
+++ graphql-schema/src/generate_schema/convert_entity.rs
@@ -39,7 +39,7 @@ pub fn convert_entity(
           position: Pos { line: 0, column: 0 },
           description: None,
           name: graphql_entity_type_name.to_owned(),
-          implements_interfaces: vec![],
+          implements_interfaces: vec!["Node".to_owned()],
           directives: vec![],
           fields: graphql_fields,
         })),
