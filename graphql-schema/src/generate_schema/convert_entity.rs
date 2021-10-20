5b3db9e22d0d87c50e8a8cc798e80fde1673b63a
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-21 00:07:53 +0100

    Add queries

--- graphql-schema/src/generate_schema/convert_entity.rs
+++ graphql-schema/src/generate_schema/convert_entity.rs
@@ -1,14 +1,27 @@
 use super::conversion_error::ConversionError;
 use super::convert_field::convert_field;
-use graphql_parser::schema::{Definition, Field, ObjectType, TypeDefinition};
+use graphql_parser::schema::{Definition, Field, ObjectType, Type, TypeDefinition};
 use graphql_parser::Pos;
 
 use rtg_model::explicit_model::ExplicitEntity;
 
-pub fn convert_entity(entity: &ExplicitEntity) -> Result<Definition<String>, ConversionError> {
+pub fn convert_entity(
+  entity: &ExplicitEntity,
+) -> Result<
+  (
+    Definition<String>,
+    Field<String>,
+    Field<String>,
+    Field<String>,
+  ),
+  ConversionError,
+> {
   match entity {
     ExplicitEntity::DatabaseTable {
       graphql_entity_type_name,
+      graphql_get_single_operation_name,
+      graphql_get_list_operation_name,
+      graphql_get_connection_operation_name,
       fields,
       ..
     } => {
@@ -21,16 +34,44 @@ pub fn convert_entity(entity: &ExplicitEntity) -> Result<Definition<String>, Con
         Err(err) => return Err(err),
       };
 
-      Ok(Definition::TypeDefinition(TypeDefinition::Object(
-        ObjectType {
+      Ok((
+        Definition::TypeDefinition(TypeDefinition::Object(ObjectType {
           position: Pos { line: 0, column: 0 },
           description: None,
           name: graphql_entity_type_name.to_owned(),
           implements_interfaces: vec![],
           directives: vec![],
           fields: graphql_fields,
+        })),
+        Field {
+          position: Pos { line: 0, column: 0 },
+          description: None,
+          name: graphql_get_single_operation_name.to_owned(),
+          arguments: vec![],
+          field_type: Type::NamedType(graphql_entity_type_name.to_owned()),
+          directives: vec![],
         },
-      )))
+        Field {
+          position: Pos { line: 0, column: 0 },
+          description: None,
+          name: graphql_get_list_operation_name.to_owned(),
+          arguments: vec![],
+          field_type: Type::NonNullType(Box::new(Type::ListType(Box::new(Type::NonNullType(
+            Box::new(Type::NamedType(graphql_entity_type_name.to_owned())),
+          ))))),
+          directives: vec![],
+        },
+        Field {
+          position: Pos { line: 0, column: 0 },
+          description: None,
+          name: graphql_get_connection_operation_name.to_owned(),
+          arguments: vec![],
+          field_type: Type::NonNullType(Box::new(Type::ListType(Box::new(Type::NonNullType(
+            Box::new(Type::NamedType(graphql_entity_type_name.to_owned())),
+          ))))),
+          directives: vec![],
+        },
+      ))
     }
   }
 }
--- graphql-schema/src/generate_schema/convert_model.rs
+++ graphql-schema/src/generate_schema/convert_model.rs
@@ -1,19 +1,37 @@
 use super::conversion_error::ConversionError;
 use super::convert_entity::convert_entity;
-use graphql_parser::schema::{Definition, Document};
+use graphql_parser::schema::{Definition, Document, ObjectType, TypeDefinition};
+use graphql_parser::Pos;
 use rtg_model::explicit_model::ExplicitModel;
 
 pub fn convert_model(model: &ExplicitModel) -> Result<Document<String>, ConversionError> {
   match model {
     ExplicitModel::V1 { entities, .. } => {
-      let graphql_types = match entities
-        .iter()
-        .map(|entity| convert_entity(entity))
-        .collect::<Result<Vec<Definition<String>>, ConversionError>>()
-      {
-        Ok(graphql_types_items) => graphql_types_items,
-        Err(err) => return Err(err),
-      };
+      let mut query_fields = vec![];
+      let mut graphql_types = vec![];
+
+      for entity in entities.iter() {
+        match convert_entity(entity) {
+          Ok(entity_definition) => {
+            graphql_types.push(entity_definition.0);
+            query_fields.push(entity_definition.1);
+            query_fields.push(entity_definition.2);
+            query_fields.push(entity_definition.3);
+          }
+          Err(err) => return Err(err),
+        }
+      }
+
+      let query_type = Definition::TypeDefinition(TypeDefinition::Object(ObjectType {
+        position: Pos { line: 0, column: 0 },
+        description: None,
+        name: "Query".to_owned(),
+        implements_interfaces: vec![],
+        directives: vec![],
+        fields: query_fields,
+      }));
+
+      graphql_types.push(query_type);
 
       Ok(Document {
         definitions: graphql_types,
